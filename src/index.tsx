1a00b74264d6141f357f75eeba5e9adfc18ed92f
AkraichLand <54623031+AkraichLand@users.noreply.github.com>
2021-10-03 00:40:53 +0000

    Refactoring out a CellCode component

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import bundle from './bundler';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState<string | undefined>('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input || 'console.log(123);');
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="console.log(123);"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
